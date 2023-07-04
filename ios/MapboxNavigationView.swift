136a9d01414c46f23e034501dda9ff0e6e5fa77e
deepak jain <deepakjain@deepaks-MacBook-Air.local>
2023-07-04 18:46:48 +0530

    Update logic for the line

--- ios/MapboxNavigationView.swift
+++ ios/MapboxNavigationView.swift
@@ -1,8 +1,8 @@
 import MapboxCoreNavigation
 import MapboxNavigation
 import MapboxDirections
-
-// // adapted from https://pspdfkit.com/blog/2017/native-view-controllers-and-react-native/ and https://github.com/mslabenyak/react-native-mapbox-navigation/blob/master/ios/Mapbox/MapboxNavigationView.swift
+import MapboxMaps
+import Turf
 extension UIView {
     var parentViewController: UIViewController? {
         var parentResponder: UIResponder? = self
@@ -15,153 +15,93 @@ extension UIView {
         return nil
     }
 }
-
 class MapboxNavigationView: UIView, NavigationViewControllerDelegate {
     weak var navViewController: NavigationViewController?
     var embedded: Bool
     var embedding: Bool
-    
     @objc var waypoints: NSArray = [] {
         didSet { setNeedsLayout() }
     }
-        
     @objc var origin: NSArray = [] {
         didSet { setNeedsLayout() }
     }
-    
     @objc var destination: NSArray = [] {
         didSet { setNeedsLayout() }
     }
-
     @objc var shouldSimulateRoute: Bool = false
     @objc var showsEndOfRouteFeedback: Bool = false
     @objc var routeLineTracksTraversal: Bool = true
     @objc var hideStatusView: Bool = false
     @objc var mute: Bool = false
-    @objc var inactiveWaypointColor: UIColor = .gray {
-        didSet {
-            if let routeStyle = navViewController?.mapView?.style {
-                routeStyle.waypointCircleColor = inactiveWaypointColor
-            }
-        }
-    }
-    
     @objc var onLocationChange: RCTDirectEventBlock?
     @objc var onRouteProgressChange: RCTDirectEventBlock?
     @objc var onError: RCTDirectEventBlock?
     @objc var onCancelNavigation: RCTDirectEventBlock?
     @objc var onArrive: RCTDirectEventBlock?
-    @objc var onSkip: RCTDirectEventBlock?
-    var button: UIButton!
-    
     override init(frame: CGRect) {
         self.embedded = false
         self.embedding = false
         super.init(frame: frame)
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        if (navViewController == nil && !embedding && !embedded) {
+        if navViewController == nil && !embedding && !embedded {
             embed()
         } else {
             navViewController?.view.frame = bounds
         }
+       // setupMapViewDelegate()
     }
-    
     override func removeFromSuperview() {
         super.removeFromSuperview()
-        // cleanup and teardown any existing resources
+        // Cleanup and teardown any existing resources
         self.navViewController?.removeFromParent()
     }
-    
     private func embed() {
         guard origin.count == 2 && destination.count == 2 else { return }
-        if ((waypoints.count >= 2) == false) {
-                     return
+        if waypoints.count < 2 {
+            return
         }
-
         embedding = true
-
-        let originWaypoint      = Waypoint(coordinate: CLLocationCoordinate2D(latitude: origin[1] as! CLLocationDegrees, longitude: origin[0] as! CLLocationDegrees))
+        let originWaypoint = Waypoint(coordinate: CLLocationCoordinate2D(latitude: origin[1] as! CLLocationDegrees, longitude: origin[0] as! CLLocationDegrees))
         let destinationWaypoint = Waypoint(coordinate: CLLocationCoordinate2D(latitude: destination[1] as! CLLocationDegrees, longitude: destination[0] as! CLLocationDegrees))
-    
         var i = 1
         var waypointsWaypoint = [originWaypoint]
-        for _ in waypoints {
-            if i%2 == 0 {
-                var wp = Waypoint(coordinate: CLLocationCoordinate2D(latitude: waypoints[i-2] as! CLLocationDegrees, longitude: waypoints[i-1] as! CLLocationDegrees))
-                waypointsWaypoint.append(wp)
-            }
-            i=i+1
+        while i < waypoints.count {
+            let wp = Waypoint(coordinate: CLLocationCoordinate2D(latitude: waypoints[i - 1] as! CLLocationDegrees, longitude: waypoints[i] as! CLLocationDegrees))
+            waypointsWaypoint.append(wp)
+            i += 2
         }
         waypointsWaypoint.append(destinationWaypoint)
         let options = NavigationRouteOptions(waypoints: waypointsWaypoint)
-
         Directions.shared.calculate(options) { [weak self] (_, result) in
             guard let strongSelf = self, let parentVC = strongSelf.parentViewController else {
                 return
             }
-            
             switch result {
-                case .failure(let error):
-                    strongSelf.onError?(["message": error.localizedDescription])
-                case .success(let response):
-                    guard let weakSelf = self else {
-                        return
-                    }
-                    
-                    let navigationService = MapboxNavigationService(routeResponse: response, routeIndex: 0, routeOptions: options, simulating: .onPoorGPS)//strongSelf.shouldSimulateRoute ? .always : .never)
-                    
-                    let navigationOptions = NavigationOptions(navigationService: navigationService)
-                    let vc = NavigationViewController(for: response, routeIndex: 0, routeOptions: options, navigationOptions: navigationOptions)
-
-                    vc.showsEndOfRouteFeedback = strongSelf.showsEndOfRouteFeedback
-                    vc.routeLineTracksTraversal = strongSelf.routeLineTracksTraversal
-                    
-                    StatusView.appearance().isHidden = strongSelf.hideStatusView
-
-                    NavigationSettings.shared.voiceMuted = strongSelf.mute;
-                    
-                    vc.delegate = strongSelf
-                
-                    parentVC.addChild(vc)
-                    strongSelf.addSubview(vc.view)
-                    vc.view.frame = strongSelf.bounds
-                    vc.didMove(toParent: parentVC)
-                    strongSelf.navViewController = vc
-                    
-                    // Add a button
-                    strongSelf.button = UIButton(type: .system)
-                    strongSelf.button.setTitle("Skip", for: .normal)
-                    strongSelf.button.addTarget(strongSelf, action: #selector(strongSelf.skipTapped), for: .touchUpInside)
-                let buttonWidth: CGFloat = 100
-                let buttonHeight: CGFloat = 40
-                let buttonX: CGFloat = strongSelf.bounds.width - buttonWidth - 20 // Adjust the padding as needed
-                let buttonY: CGFloat = 40 // Adjust the top padding as needed
-
-                strongSelf.button.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
-                vc.view.addSubview(strongSelf.button)
-                    
-                    // Set the line color for inactive waypoints
-                    if let routeStyle = vc.mapView?.style {
-                        routeStyle.waypointCircleColor = strongSelf.inactiveWaypointColor
-                    }
+            case .failure(let error):
+                strongSelf.onError?(["message": error.localizedDescription])
+            case .success(let response):
+                let navigationService = MapboxNavigationService(routeResponse: response, routeIndex: 0, routeOptions: options, simulating: strongSelf.shouldSimulateRoute ? .always : .onPoorGPS)
+                let navigationOptions = NavigationOptions(navigationService: navigationService)
+                let vc = NavigationViewController(for: response, routeIndex: 0, routeOptions: options, navigationOptions: navigationOptions)
+                vc.showsEndOfRouteFeedback = strongSelf.showsEndOfRouteFeedback
+                vc.routeLineTracksTraversal = strongSelf.routeLineTracksTraversal
+                StatusView.appearance().isHidden = strongSelf.hideStatusView
+                NavigationSettings.shared.voiceMuted = strongSelf.mute;
+                vc.delegate = strongSelf
+                parentVC.addChild(vc)
+                strongSelf.addSubview(vc.view)
+                vc.view.frame = strongSelf.bounds
+                vc.didMove(toParent: parentVC)
+                strongSelf.navViewController = vc
             }
             strongSelf.embedded = true
         }
     }
-    
-    @objc func skipTapped() {
-        // Handle button tap event here
-        onSkip?(["message": ""]);
-    }
-    
     func navigationViewController(_ navigationViewController: NavigationViewController, didUpdate progress: RouteProgress, with location: CLLocation, rawLocation: CLLocation) {
         onLocationChange?(["longitude": location.coordinate.longitude, "latitude": location.coordinate.latitude])
         onRouteProgressChange?(["distanceTraveled": progress.distanceTraveled,
@@ -169,16 +109,76 @@ class MapboxNavigationView: UIView, NavigationViewControllerDelegate {
                                 "fractionTraveled": progress.fractionTraveled,
                                 "distanceRemaining": progress.distanceRemaining])
     }
-    
     func navigationViewControllerDidDismiss(_ navigationViewController: NavigationViewController, byCanceling canceled: Bool) {
-        if (!canceled) {
-            return;
+        if canceled {
+            onCancelNavigation?(["message": ""])
         }
-        onCancelNavigation?(["message": ""]);
     }
-    
+    private func setupMapViewDelegate() {
+        if let navViewController = navViewController, let mapView = navViewController.navigationMapView {
+           // mapView.delegate = self
+        }
+    }
     func navigationViewController(_ navigationViewController: NavigationViewController, didArriveAt waypoint: Waypoint) -> Bool {
-        onArrive?(["message": ""]);
-        return true;
+        onArrive?(["message": ""])
+        return true
     }
+}
+
+extension MapboxNavigationView: NavigationMapViewDelegate {
+ 
+func lineWidthExpression(_ multiplier: Double = 1.0) -> Expression {
+let lineWidthExpression = Exp(.interpolate) {
+Exp(.linear)
+Exp(.zoom)
+// It's possible to change route line width depending on zoom level, by using expression
+// instead of constant. Navigation SDK for iOS also exposes `RouteLineWidthByZoomLevel`
+// public property, which contains default values for route lines on specific zoom levels.
+RouteLineWidthByZoomLevel.multiplied(by: multiplier)
+}
+ 
+return lineWidthExpression
+}
+
+ 
+// It's possible to change route line shape in preview mode by adding own implementation to either
+// `NavigationMapView.navigationMapView(_:shapeFor:)` or `NavigationMapView.navigationMapView(_:casingShapeFor:)`.
+func navigationMapView(_ navigationMapView: NavigationMapView, shapeFor route: Route) -> LineString? {
+return route.shape
+}
+ 
+func navigationMapView(_ navigationMapView: NavigationMapView, casingShapeFor route: Route) -> LineString? {
+return route.shape
+}
+ 
+func navigationMapView(_ navigationMapView: NavigationMapView, routeLineLayerWithIdentifier identifier: String, sourceIdentifier: String) -> LineLayer? {
+var lineLayer = LineLayer(id: identifier)
+lineLayer.source = sourceIdentifier
+ 
+// `identifier` parameter contains unique identifier of the route layer or its casing.
+// Such identifier consists of several parts: unique address of route object, whether route is
+// main or alternative, and whether route is casing or not. For example: identifier for
+// main route line will look like this: `0x0000600001168000.main.route_line`, and for
+// alternative route line casing will look like this: `0x0000600001ddee80.alternative.route_line_casing`.
+lineLayer.lineColor = .constant(.init(identifier.contains("main") ? #colorLiteral(red: 0.337254902, green: 0.6588235294, blue: 0.9843137255, alpha: 1) : #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)))
+lineLayer.lineWidth = .expression(lineWidthExpression())
+lineLayer.lineJoin = .constant(.round)
+lineLayer.lineCap = .constant(.round)
+ 
+return lineLayer
+}
+ 
+func navigationMapView(_ navigationMapView: NavigationMapView, routeCasingLineLayerWithIdentifier identifier: String, sourceIdentifier: String) -> LineLayer? {
+var lineLayer = LineLayer(id: identifier)
+lineLayer.source = sourceIdentifier
+ 
+// Based on information stored in `identifier` property (whether route line is main or not)
+// route line will be colored differently.
+lineLayer.lineColor = .constant(.init(identifier.contains("main") ? #colorLiteral(red: 0.1843137255, green: 0.4784313725, blue: 0.7764705882, alpha: 1) : #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)))
+lineLayer.lineWidth = .expression(lineWidthExpression(1.2))
+lineLayer.lineJoin = .constant(.round)
+lineLayer.lineCap = .constant(.round)
+ 
+return lineLayer
+}
 }
